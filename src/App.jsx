b73bd1365e3590a458d26bbe8e6a38bf9c46fded
LAPTOP-5RU3UB56\Berke <berken_93@hotmail.com>
2025-02-27 09:54:43 +0100

    added link and anchors to test page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom"
 
 function App() {
   return (
     <>
       <h1>I have succesfully deplyed </h1>
+      <Link to={"/test"}>To test page</Link>
+      <a href="/test">Anchor to test page</a>
     </>
   )
 }
